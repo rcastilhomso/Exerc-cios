c7eeb1ef4689a8f2ee2af7ce56e6cc84387755e5
raphael.castilho <raphael.castilho@pebmed.com.br>
2022-12-20 18:27:16 -0300

    atualização 20.12.2022

--- tela_login/js/login.js
+++ tela_login/js/login.js
@@ -1,6 +1,5 @@
 const registerBtn = document.getElementById("register-btn");
 const loginBtn = document.getElementById("login-btn");
-import User from "./user.js";
 
 registerBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -12,7 +11,8 @@ registerBtn.addEventListener("click", (e) => {
     return;
   }
   const user = new User(email, password);
-  localStorage.setItem(`user`, { user });
+  localStorage.setItem(`email`, email);
+  localStorage.setItem(`password`, password);
   document.getElementById("new-email").value = "";
   document.getElementById("new-password").value = "";
   document.getElementById("overlay").style.display = "none";
@@ -22,19 +22,21 @@ loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const user = localStorage.getItem("user");
+  const localStorageEmail = localStorage.getItem("email");
+  const localStoragePassword = localStorage.getItem("password");
 
   if (email === "" || password === "") {
     alert("Preencha todos os campos!");
     return;
   }
 
-  if (email !== user.email || password !== user.password) {
+  if (email !== localStorageEmail || password !== localStoragePassword) {
     alert("Usuário ou senha incorretos!");
     return;
   }
 
   alert("Login realizado com sucesso!");
+  window.location.href = "./home.html";
   document.getElementById("email").value = "";
   document.getElementById("password").value = "";
 });
